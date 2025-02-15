60f7526820aab3696540e9ee53db1c90270e82d0
Pralay Pal <pralaypal111@gmail.com>
2025-02-15 20:34:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Fullstack Developer", "Web Designer", "Freelancer"], 
+        typeSpeed: 100,
+        loop: true,
+        backSpeed: 50,
+        backDelay: 1000,
+        startDelay: 1000
+    });
+});
